7d7220c6d447f718f98e1b03724b9badaeef55f1
Ilya <156573248+islepets@users.noreply.github.com>
2025-05-03 16:42:38 +0400

    lab2

---
+++ lab2_os/schedule_priority.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "list.h"
+#include "schedulers.h"
+#include "task.h"
+#include "cpu.h"
+
+struct node *taskList = NULL;
+
+void add(char *name, int priority, int burst) {
+    Task *newTask = malloc(sizeof(Task));
+    if (newTask == NULL) {
+        fprintf(stderr, "Memory allocation failed\n");
+        exit(1);
+    }
+    
+    newTask->name = strdup(name);
+    if (newTask->name == NULL) {
+        fprintf(stderr, "String duplication failed\n");
+        free(newTask);  
+        exit(1);  
+    }
+    
+    newTask->priority = priority;
+    newTask->burst = burst;
+
+    insert(&taskList, newTask);
+}
+
+
+void schedule() {
+    struct node *current = taskList;
+    while (current != NULL) {
+        Task *task = current->task;
+        run(task, task->burst);
+        current = current->next;
+    }
+}
---
+++ lab2_os/schedule_priority_rr.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "list.h"
+#include "schedulers.h"
+#include "task.h"
+#include "cpu.h"
+
+struct node *taskList = NULL;
+
+void add(char *name, int priority, int burst) {
+    Task *newTask = malloc(sizeof(Task));
+    if (newTask == NULL) {
+        perror("Failed to allocate memory for new task");
+        exit(EXIT_FAILURE);
+    }
+    newTask->name = strdup(name);
+    if (newTask->name == NULL) {
+        perror("Failed to allocate memory for task name");
+        free(newTask);
+        exit(EXIT_FAILURE);
+    }
+    newTask->priority = priority;
+    newTask->burst = burst;
+
+    insert(&taskList, newTask);
+}
+
+void schedule() {
+
+    struct node *current = taskList;
+
+    while (current != NULL) {
+        Task *task = current->task;
+        int timeSlice = (task->burst > QUANTUM) ? QUANTUM : task->burst;
+        run(task, timeSlice);
+        task->burst -= timeSlice;
+
+        if (task->burst <= 0) {
+            delete(&taskList, task);
+        }
+        current = current->next;
+        if (current == NULL) {
+            current = taskList;
+        }
+    }
+}
